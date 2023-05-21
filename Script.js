b57eadb5209e33a4bd93376b871cea7587d1cc3c
Ivan3750 <126425359+Ivan3750@users.noreply.github.com>
2023-05-21 16:56:21 +0200

    Ravel

---
+++ Script.js
@@ -0,0 +1,27 @@
+
+const dist__btn__left = document.querySelector(".dist__btn__left");
+const dist__photo = document.querySelector(".dist-photo-1");
+
+dist__btn__left.addEventListener("click", () => {
+    dist__photo.classList.toggle("dist__photo__152");
+
+});
+
+ /* span.material-symbols-outlined{
+    color: #FF5B65;
+    width: 54px;
+    height: 54px;
+}
+
+.material-symbols-outlined:nth-child(3){
+    width: 54px;
+    height: 54px;
+    color:#FF5B65;
+}
+.material-symbols-outlined:nth-child(4) {
+    width: 50px;
+    height: 50px;
+    color: #FF5B65;
+}
+
+ */
